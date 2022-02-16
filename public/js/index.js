b708a63eb1eb8ddba996e15536e7276cdaf53a90
Ograeset <tedros963@gmail.com>
2022-02-16 11:24:33 +0100

    correct typos

--- public/js/index.js
+++ public/js/index.js
@@ -7,7 +7,7 @@
   row.innerHTML = "<td>data1</td><td>data2</td><td>data3</td>";
 }*/
 
-var anElement = doocument.getElementById("app")
+var anElement = document.getElementById("app")
 
 anElement.innerHTML = "Hello world"
 anElement.outerHTML = "Hello world"
@@ -28,7 +28,7 @@ function age(){
 function createTable(data){
   var appElement = document.getElementById("app")
   var aTable = document.createElement("table")
-  apppElement.appendChild(aTable)
+  appElement.appendChild(aTable)
   aTable.appendChild(createRow(data[0].name, data[0].points))
   aTable.appendChild(createRow(data[1].name, data[1].points))
   aTable.appendChild(createRow(data[2].name, data[2].points))
